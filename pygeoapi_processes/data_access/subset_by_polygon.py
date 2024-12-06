21a34bc2751fcb3665596e4fedd76a7c9bd2e306
Merret <merret.buurman@igb-berlin.de>
2024-12-07 00:24:24 +0100

    subset_by_bbox, subset_by_polygon: Commented failing imports, dependency problems.

--- pygeoapi_processes/data_access/subset_by_polygon.py
+++ pygeoapi_processes/data_access/subset_by_polygon.py
@@ -1,43 +1,29 @@
-# =================================================================
-#
-# Authors: Tom Kralidis <tomkralidis@gmail.com>
-#
-# Copyright (c) 2022 Tom Kralidis
-#
-# Permission is hereby granted, free of charge, to any person
-# obtaining a copy of this software and associated documentation
-# files (the "Software"), to deal in the Software without
-# restriction, including without limitation the rights to use,
-# copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following
-# conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-#
-# =================================================================
+
+####################################
+### This process currently fails ###
+### due to import gdal issues!   ###
+####################################
 
 import logging
+import json
 import traceback
 import os
-import rasterio
-import rasterio.mask
-from osgeo import gdal
-import json
 import requests
-from pygeoapi.process.aqua90m.utils.raster_helpers import compress_tiff
+
+# pygeoapi-related modules:
 from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
 
+# geo-related modules:
+import rasterio
+import rasterio.mask
+
+# We need one of these: # TODO FIXME, does not currently work on our server due to
+# dependency hell, ever since I had to reinstall gdal when installing hydrographr.
+from osgeo import gdal # May cause: ModuleNotFoundError: No module named '_gdal'
+import gdal as gdal    # May cause: ModuleNotFoundError: No module named 'gdal'
+
+# our own helpers:
+from pygeoapi.process.aqua90m.utils.raster_helpers import compress_tiff
 
 
 '''
