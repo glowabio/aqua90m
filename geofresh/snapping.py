4ae9d63f5d48c60a9b91998f0640c2dd36ca3835
Merret <merret.buurman@igb-berlin.de>
2025-02-25 07:31:57 +0100

    First version of plural snapping, using temp table. Not so fast.

--- geofresh/snapping.py
+++ geofresh/snapping.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 import geomet.wkt
 import logging
 LOGGER = logging.getLogger(__name__)
@@ -6,11 +7,17 @@ LOGGER = logging.getLogger(__name__)
 # TODO: FUTURE: If we ever snap to stream segments outside of the immediate subcatchment,
 # need to adapt some stuff in this process...
 
+
+###########################
+### One point at a time ###
+###########################
+
 def get_snapped_point_geometry_coll(conn, lon, lat, subc_id, basin_id, reg_id):
     return _get_snapped_point_plus(conn, lon, lat, subc_id, basin_id, reg_id, make_feature = False)
 
 def get_snapped_point_feature_coll(conn, lon, lat, subc_id, basin_id, reg_id):
     return _get_snapped_point_plus(conn, lon, lat, subc_id, basin_id, reg_id, make_feature = True)
+
 
 def _get_snapped_point_plus(conn, lon, lat, subc_id, basin_id, reg_id, make_feature = False):
     ### Define query:
@@ -137,6 +144,7 @@ def _get_snapped_point_plus(conn, lon, lat, subc_id, basin_id, reg_id, make_feat
 
     return snappedpoint_feature
 
+
 def get_snapped_point_feature(conn, lon, lat, subc_id, basin_id, reg_id):
 
     ### Define query:
@@ -203,7 +211,7 @@ def get_snapped_point_feature(conn, lon, lat, subc_id, basin_id, reg_id):
 
     return snappedpoint_feature
 
-#def get_snapped_point_simple(conn, lon, lat, subc_id, basin_id, reg_id):
+
 def get_snapped_point_simplegeom(conn, lon, lat, subc_id, basin_id, reg_id):
     """
     Example result:
@@ -257,6 +265,124 @@ def get_snapped_point_simplegeom(conn, lon, lat, subc_id, basin_id, reg_id):
     snappedpoint_simplegeom = geomet.wkt.loads(row[0])
     return snappedpoint_simplegeom
 
+
+#############################
+### Many points at a time ###
+#############################
+
+def _create_temp_table_of_user_points(cursor, tablename, input_points_geojson):
+
+    LOGGER.debug('Creating temporary table "%s"...' % tablename)
+
+    ## Create temp table: # TODO WIP numeric or decimal or ...?
+    query_create = """
+    CREATE TEMP TABLE {tablename} (
+    lon decimal,
+    lat decimal,
+    subc_id integer,
+    basin_id integer,
+    reg_id smallint,
+    geom_user geometry(POINT, 4326)
+    );
+    """.format(tablename = tablename)
+    query_create = query_create.replace("\n", " ")
+
+    ## Insert the user values
+    tmp = []
+    for lon, lat in input_points_geojson["coordinates"]:
+        tmp.append("({lon}, {lat}, ST_SetSRID(ST_MakePoint({lon}, {lat}), 4326))".format(lon=lon, lat=lat))
+
+    query_insert = "INSERT INTO {tablename}(lon, lat, geom_user) VALUES {values};".format(tablename=tablename, values=", ".join(tmp))
+
+    ## Add reg_id and subc_id:
+    query_reg = "UPDATE {tablename} SET reg_id = reg.reg_id FROM regional_units reg WHERE st_intersects({tablename}.geom_user, reg.geom);".format(tablename = tablename)
+    query_sub_bas = "UPDATE {tablename} SET subc_id = sub.subc_id, basin_id = sub.basin_id FROM sub_catchments sub WHERE st_intersects({tablename}.geom_user, sub.geom) AND {tablename}.reg_id = sub.reg_id;".format(tablename = tablename)
+    cursor.execute(query_create + query_insert + query_reg + query_sub_bas)
+
+    LOGGER.debug('Creating temporary table "%s"... DONE.' % tablename)
+
+
+def get_snapped_point_feature_coll_plural(conn, input_points_geojson):
+
+    tablename = 'snapping_{uuid}'.format(uuid = str(uuid.uuid4()).replace('-', ''))
+    cursor = conn.cursor()
+    _create_temp_table_of_user_points(cursor, tablename, input_points_geojson)
+
+    ## Run the snapping query
+    ## (which does not write anything into the database):
+    query = '''
+    SELECT
+    poi.lon,
+    poi.lat,
+    poi.subc_id,
+    poi.basin_id,
+    poi.reg_id,
+    seg.strahler,
+    ST_AsText(ST_LineInterpolatePoint(seg.geom, ST_LineLocatePoint(seg.geom, poi.geom_user)))
+    FROM hydro.stream_segments seg, {tablename} poi
+    WHERE seg.subc_id = poi.subc_id AND seg.reg_id = poi.reg_id;
+    '''.format(tablename = tablename)
+    # TODO: Add ST_AsText(seg.geom) if you want the linestring!
+    query = query.replace("\n", " ")
+    LOGGER.debug('Querying database with snapping query...')
+    cursor.execute(query)
+    LOGGER.debug('Querying database with snapping query... DONE.')
+
+    ## Now iterate over result rows:
+    LOGGER.debug('Iterating over the result rows, constructing GeoJSON...')
+    features = []
+    while (True):
+        row = cursor.fetchone()
+        if row is None: break
+
+        # Extract values from row:
+        lon = float(row[0])
+        lat = float(row[1])
+        subc_id = row[2]
+        basin_id = row[3]
+        reg_id = row[4]
+        strahler = row[5]
+        snappedpoint_wkt = row[6]
+        #streamsegment_wkt = row[7]
+
+        # Convert to GeoJSON:
+        snappedpoint_simplegeom = geomet.wkt.loads(snappedpoint_wkt)
+        #streamsegment_linestring = geomet.wkt.loads(streamsegment_wkt)
+
+        # Construct Feature, incl. ids, strahler and original lonlat:
+        # TODO: If all are in same reg_id and basin, we could remove those
+        # attributes from here...
+        features.append({
+            "type": "Feature",
+            "geometry": snappedpoint_simplegeom,
+            "properties": {
+                "subc_id": subc_id,
+                "strahler": strahler,
+                "basin_id": basin_id,
+                "reg_id": reg_id,
+                "lon_original": lon,
+                "lat_original": lat,
+            }
+        })
+
+
+    LOGGER.debug('Iterating over the result rows, constructing GeoJSON... DONE.')
+
+    if len(features) == 0:
+        raise ValueError("No features...")
+
+    feature_coll = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    ## Drop temp table:
+    LOGGER.debug('Dropping temporary table "%s".' % tablename)
+    query_drop = "DROP TABLE IF EXISTS {tablename};".format(tablename = tablename)
+    cursor.execute(query_drop)
+
+    LOGGER.debug('Returning GeoJSON: %s' % feature_coll)
+    return feature_coll
 
 
 if __name__ == "__main__":
@@ -319,3 +445,40 @@ if __name__ == "__main__":
     print('\nSTART RUNNING FUNCTION: get_snapped_point_feature_coll')
     res = get_snapped_point_feature_coll(conn, lon, lat, subc_id, basin_id, reg_id)
     print('RESULT:\n%s' % res)
+
+    ############
+    ### Many ###
+    ############
+
+    input_points_geojson = {
+        "type": "MultiPoint",
+        "coordinates": [
+            [9.931555, 54.695070],
+            [9.921555, 54.295070]
+        ]
+    }
+    '''
+    TODO: SHould we allow people to pass in FeatureCollections, and try to keep the properties?
+    input_points_geojson = {
+        "type": "FeatureCollection",
+        "features": [{
+           "type": "Feature",
+           "geometry": { "type": "Point", "coordinates": [9.931555, 54.695070]},
+           "properties": {
+               "species_name": "Hase",
+               "species_id": "007"
+           }
+        },
+        {
+           "type": "Feature",
+           "geometry": { "type": "Point", "coordinates": [9.921555, 54.295070]},
+           "properties": {
+               "species_name": "Delphin",
+               "species_id": "008"
+           }
+        }]
+    }
+    '''
+    print('\nSTART RUNNING FUNCTION: get_snapped_point_feature_coll_plural')
+    res = get_snapped_point_feature_coll_plural(conn, input_points_geojson)
+    print('RESULT: %s' % res)
---
+++ pygeoapi_processes/rivernetwork/get_snapped_points_plural.py
@@ -0,0 +1,181 @@
+import logging
+from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+LOGGER = logging.getLogger(__name__)
+
+import argparse
+import os
+import sys
+import traceback
+import json
+import psycopg2
+from pygeoapi.process.aqua90m.geofresh.py_query_db import get_connection_object
+import pygeoapi.process.aqua90m.geofresh.basic_queries as basic_queries
+import pygeoapi.process.aqua90m.geofresh.snapping as snapping
+
+
+'''
+curl -X POST "http://localhost:5000/pygeoapi/processes/get-snapped-points-plural/execution" \
+--header "Content-Type: application/json" \
+--data '{
+  "inputs": {
+    "points": {
+      "type": "MultiPoint",
+      "coordinates": [
+        [9.937520027160646, 54.69422745526058],
+        [9.9217, 54.6917],
+        [9.9312, 54.6933]
+      ]
+    }
+  }
+}'
+'''
+
+# Process metadata and description
+# Has to be in a JSON file of the same name, in the same dir! 
+script_title_and_path = __file__
+metadata_title_and_path = script_title_and_path.replace('.py', '.json')
+PROCESS_METADATA = json.load(open(metadata_title_and_path))
+
+
+class SnappedPointsGetterPlural(BaseProcessor):
+
+    def __init__(self, processor_def):
+        super().__init__(processor_def, PROCESS_METADATA)
+        self.supports_outputs = True
+        self.job_id = None
+        self.config = None
+
+        # Set config:
+        config_file_path = os.environ.get('AQUA90M_CONFIG_FILE', "./config.json")
+        with open(config_file_path, 'r') as config_file:
+            self.config = json.load(config_file)
+
+
+    def set_job_id(self, job_id: str):
+        self.job_id = job_id
+
+
+    def __repr__(self):
+        return f'<SnappedPointsGetterPlural> {self.name}'
+
+
+    def execute(self, data, outputs=None):
+        LOGGER.info('Starting to get the snapped point coordinates..."')
+        LOGGER.info('Inputs: %s' % data)
+        LOGGER.info('Requested outputs: %s' % outputs)
+        try:
+            conn = self.get_db_connection()
+            res = self._execute(data, outputs, conn)
+
+            LOGGER.debug('Closing connection...')
+            conn.close()
+            LOGGER.debug('Closing connection... Done.')
+
+            return res
+
+        except psycopg2.Error as e3:
+            conn.close()
+            err = f"{type(e3).__module__.removesuffix('.errors')}:{type(e3).__name__}: {str(e3).rstrip()}"
+            error_message = 'Database error: %s (%s)' % (err, str(e3))
+            LOGGER.error(error_message)
+            raise ProcessorExecuteError(user_msg = error_message)
+
+        except Exception as e:
+            conn.close()
+            LOGGER.error('During process execution, this happened: %s' % e)
+            print(traceback.format_exc())
+            raise ProcessorExecuteError(e) # TODO: Can we feed e into ProcessExecuteError?
+
+
+    def _execute(self, data, requested_outputs, conn):
+
+        # User inputs
+        input_points_geojson = data.get('points')
+        geometry_only = data.get('geometry_only', 'false')
+        comment = data.get('comment') # optional
+
+        # Parse booleans
+        geometry_only = (geometry_only.lower() == 'true')
+
+        if geometry_only:
+            raise ProcessorExecuteError("Not implemented yet, please set geometry_only to False.")
+
+        if not geometry_only:
+            feature_coll = snapping.get_snapped_point_feature_coll_plural(conn, input_points_geojson)
+            LOGGER.debug('Received feature collection: %s' % feature_coll)
+
+            ## Add comment:
+            if comment is not None:
+                feature_coll['comment'] = comment
+
+            if self.return_hyperlink('snapped_points', requested_outputs):
+                LOGGER.debug('Will return file:')
+                return 'application/json', self.store_to_json_file('snapped_points', feature_coll)
+            else:
+                LOGGER.debug('Will return feature collection: %s' % feature_coll)
+                return 'application/json', feature_coll
+
+
+
+    def return_hyperlink(self, output_name, requested_outputs):
+
+        if requested_outputs is None:
+            return False
+
+        if 'transmissionMode' in requested_outputs.keys():
+            if requested_outputs['transmissionMode'] == 'reference':
+                return True
+
+        if output_name in requested_outputs.keys():
+            if 'transmissionMode' in requested_outputs[output_name]:
+                if requested_outputs[output_name]['transmissionMode'] == 'reference':
+                    return True
+
+        return False
+
+
+    def store_to_json_file(self, output_name, json_object):
+
+        # Store to file
+        downloadfilename = 'outputs-%s-%s.json' % (self.metadata['id'], self.job_id)
+        downloadfilepath = self.config['download_dir']+downloadfilename
+        LOGGER.debug('Writing process result to file: %s' % downloadfilepath)
+        with open(downloadfilepath, 'w', encoding='utf-8') as downloadfile:
+            json.dump(json_object, downloadfile, ensure_ascii=False, indent=4)
+
+        # Create download link:
+        downloadlink = self.config['download_url'] + downloadfilename
+
+        # Create output to pass back to user
+        outputs_dict = {
+            'title': self.metadata['outputs'][output_name]['title'],
+            'description': self.metadata['outputs'][output_name]['description'],
+            'href': downloadlink
+        }
+
+        return outputs_dict
+
+
+    def get_db_connection(self):
+
+        config = self.config
+
+        geofresh_server = config['geofresh_server']
+        geofresh_port = config['geofresh_port']
+        database_name = config['database_name']
+        database_username = config['database_username']
+        database_password = config['database_password']
+        use_tunnel = config.get('use_tunnel')
+        ssh_username = config.get('ssh_username')
+        ssh_password = config.get('ssh_password')
+        localhost = config.get('localhost')
+
+        try:
+            conn = get_connection_object(geofresh_server, geofresh_port,
+                database_name, database_username, database_password,
+                use_tunnel=use_tunnel, ssh_username=ssh_username, ssh_password=ssh_password)
+        except sshtunnel.BaseSSHTunnelForwarderError as e1:
+            LOGGER.error('SSH Tunnel Error: %s' % str(e1))
+            raise e1
+
+        return conn
