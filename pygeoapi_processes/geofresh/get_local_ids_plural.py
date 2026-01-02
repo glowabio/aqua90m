8b27b969fa816de646476b8c90f1a9eae968dcee
Merret Buurman <merret.buurman@igb-berlin.de>
2026-01-02 21:42:50 +0100

    Second process to get the base class GeoFreshBaseProcessor: get-local-ids-plural

--- pygeoapi_processes/geofresh/get_local_ids_plural.py
+++ pygeoapi_processes/geofresh/get_local_ids_plural.py
@@ -17,6 +17,7 @@ import pygeoapi.process.aqua90m.geofresh.basic_queries as basic_queries
 import pygeoapi.process.aqua90m.utils.geojson_helpers as geojson_helpers
 import pygeoapi.process.aqua90m.utils.exceptions as exc
 import pygeoapi.process.aqua90m.pygeoapi_processes.utils as utils
+from pygeoapi.process.aqua90m.pygeoapi_processes.geofresh.GeoFreshBaseProcessor import GeoFreshBaseProcessor
 from pygeoapi.process.aqua90m.geofresh.database_connection import get_connection_object_config
 
 
@@ -116,57 +117,10 @@ metadata_title_and_path = script_title_and_path.replace('.py', '.json')
 PROCESS_METADATA = json.load(open(metadata_title_and_path))
 
 
-class LocalIdGetterPlural(BaseProcessor):
+class LocalIdGetterPlural(GeoFreshBaseProcessor):
 
     def __init__(self, processor_def):
         super().__init__(processor_def, PROCESS_METADATA)
-        self.supports_outputs = True
-        self.job_id = None
-        self.config = None
-
-        # Set config:
-        config_file_path = os.environ.get('AQUA90M_CONFIG_FILE', "./config.json")
-        with open(config_file_path, 'r') as config_file:
-            self.config = json.load(config_file)
-            self.download_dir = self.config['download_dir']
-            self.download_url = self.config['download_url']
-
-
-    def set_job_id(self, job_id: str):
-        self.job_id = job_id
-
-
-    def __repr__(self):
-        return f'<LocalIdGetterPlural> {self.name}'
-
-
-    def execute(self, data, outputs=None):
-        LOGGER.debug('Start execution: %s (job %s)' % (self.metadata['id'], self.job_id))
-        LOGGER.debug('Inputs: %s' % data)
-        LOGGER.log(logging.TRACE, 'Requested outputs: %s' % outputs)
-
-        try:
-            conn = get_connection_object_config(self.config)
-            res = self._execute(data, outputs, conn)
-            LOGGER.debug('Finished execution: %s (job %s)' % (self.metadata['id'], self.job_id))
-            LOGGER.log(5, 'Closing connection...')
-            conn.close()
-            LOGGER.log(5, 'Closing connection... Done.')
-            return res
-
-        except psycopg2.Error as e3:
-            conn.close()
-            err = f"{type(e3).__module__.removesuffix('.errors')}:{type(e3).__name__}: {str(e3).rstrip()}"
-            error_message = 'Database error: %s (%s)' % (err, str(e3))
-            LOGGER.error(error_message)
-            raise ProcessorExecuteError(user_msg = error_message)
-
-        except Exception as e:
-            conn.close()
-            LOGGER.error('During process execution, this happened: %s' % e)
-            print(traceback.format_exc())
-            raise ProcessorExecuteError(e) # TODO: Can we feed e into ProcessExecuteError?
-
 
     def _execute(self, data, requested_outputs, conn):
 
@@ -317,36 +271,5 @@ class LocalIdGetterPlural(BaseProcessor):
         ### Return result ###
         #####################
 
-        do_return_link = utils.return_hyperlink('local_ids', requested_outputs)
+        return self.return_results('local_ids', requested_outputs, output_df=output_df, output_json=output_json, comment=comment)
 
-        ## Return CSV:
-        if output_df is not None:
-            if do_return_link:
-                output_dict_with_url =  utils.store_to_csv_file('local_ids', output_df,
-                    self.metadata, self.job_id,
-                    self.download_dir,
-                    self.download_url)
-
-                if comment is not None:
-                    output_dict_with_url['comment'] = comment
-
-                return 'application/json', output_dict_with_url
-            else:
-                err_msg = 'Not implemented return CSV data directly.'
-                LOGGER.error(err_msg)
-                raise NotImplementedError(err_msg)
-
-        ## Return JSON:
-        elif output_json is not None:
-            if comment is not None:
-                output_json['comment'] = comment
-
-            if do_return_link:
-                output_dict_with_url =  utils.store_to_json_file('local_ids', output_json,
-                    self.metadata, self.job_id,
-                    self.download_dir,
-                    self.download_url)
-                return 'application/json', output_dict_with_url
-
-            else:
-                return 'application/json', output_json
