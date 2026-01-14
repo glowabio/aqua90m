5c06cf041efcf00236e65e137a2e3a257fef8acb
Merret <merret.buurman@igb-berlin.de>
2026-01-14 16:43:58 +0100

    Base class for all processors: Improved handling of Database errors.

--- pygeoapi_processes/geofresh/GeoFreshBaseProcessor.py
+++ pygeoapi_processes/geofresh/GeoFreshBaseProcessor.py
@@ -43,6 +43,7 @@ class GeoFreshBaseProcessor(BaseProcessor):
         LOGGER.debug(f'Start execution: {self.process_id} (job {self.job_id})')
         LOGGER.debug(f'Inputs: {data}')
         LOGGER.log(logging.TRACE, 'Requested outputs: {outputs}')
+        conn = None # Needed in case exceptino is raised during get_connection_object_config()
 
         try:
             conn = get_connection_object_config(self.config)
@@ -54,14 +55,63 @@ class GeoFreshBaseProcessor(BaseProcessor):
             return res
 
         except psycopg2.Error as e3:
-            conn.close()
-            err = f"{type(e3).__module__.removesuffix('.errors')}:{type(e3).__name__}: {str(e3).rstrip()}"
-            error_message = f'Database error: {err} ({str(e3)})'
-            LOGGER.error(error_message)
-            raise ProcessorExecuteError(user_msg = error_message)
+            # Prepare error messages for user (understandable) and for log (detailed)
+            err_msg_log = 'no message.'
+
+            # Make user-safe messages from various database exceptions:
+            if isinstance(e3, psycopg2.OperationalError):
+                err_msg_user = "Temporarily unavailable (details hidden, please ask admin/see log)"
+                # Examples:
+                # psycopg2:OperationalError: connection to server at "172.16.4.76", port 5432 failed: Connection refused
+                # psycopg2:OperationalError: SSL SYSCALL error: EOF detected (SSL SYSCALL error: EOF detected
+                # psycopg2:OperationalError: could not translate host name "172.16.4.7666" to address: Name or service not known
+                # psycopg2:OperationalError: invalid port number: "543222"
+                # psycopg2:OperationalError: connection to server at "172.16.4.76", port 5432 failed: FATAL:  no pg_hba.conf entry for host "172.16.8.145", user "shiny_user", database "geofreshihh_data", SSL encryption
+                # psycopg2:OperationalError: connection to server at "172.16.4.76", port 5432 failed: FATAL:  no pg_hba.conf entry for host "172.16.8.145", user "shinyy_user", database "geofresh_data", SSL encryption
+                # psycopg2:OperationalError: connection to server at "172.16.4.76", port 5432 failed: FATAL:  password authentication failed for user "shiny_user"
+            elif isinstance(e3, psycopg2.errors.QueryCanceled):
+                err_msg_user = "Request timed out (details hidden, please ask admin/see log)"
+            elif isinstance(e3, psycopg2.IntegrityError):
+                err_msg_user = "Resource conflict (details hidden, please ask admin/see log)"
+            elif isinstance(e3, psycopg2.DataError):
+                err_msg_user = "Invalid input (details hidden, please ask admin/see log)"
+            else:
+                # For example:
+                #psycopg2.InterfaceError
+                #psycopg2.InternalError
+                #psycopg2.ProgrammingError
+                #psycopg2.errors.SerializationFailure
+                #psycopg2.errors.SerializationFailure
+                #psycopg2.errors.InsufficientPrivilege
+                err_msg_user = 'Database error (details hidden, please ask admin/see log).'
+
+            # Collect details about error for log:
+            module_name = type(e3).__module__.removesuffix('.errors')
+            exception_name = type(e3).__name__
+            exception_message = str(e3).rstrip()
+            exception_full = f"{module_name}:{exception_name}: {exception_message}"
+            # Example: psycopg2:OperationalError: connection to server at "172.16.4.76",
+            # port 5432 failed: Connection refused. Is the server running on that host
+            # and accepting TCP/IP connections?
+
+            # Check if connection was opened and has to be closed, or never opened:
+            if conn is not None:
+                err_msg_log = f"Initial connection to GeoFRESH database successful, but: {exception_full}"
+                LOGGER.debug('Now closing connection (as part of error handling)')
+                conn.close()
+                LOGGER.debug('Database connection was closed by us (as part of error handling).')
+            else:
+                err_msg_user = f"Was not able to connect to the database: {err_msg_user}"
+                err_msg_log = f"Not able to connect to GeoFRESH database: {exception_full}"
+
+            # Log and raise:
+            LOGGER.error(err_msg_log)
+            err_msg_user = f"Database error: {err_msg_user}"
+            raise ProcessorExecuteError(user_msg = err_msg_user)
 
         except Exception as e:
-            conn.close()
+            if conn is not None:
+                conn.close()
             LOGGER.error(f'During process execution, this happened: {repr(e)}')
             print(traceback.format_exc())
             raise ProcessorExecuteError(e) # TODO: Can we feed e into ProcessExecuteError?
