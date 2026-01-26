d081e7aabc86dd6edd788886b0b9d816fc179401
Merret Buurman <merret.buurman@igb-berlin.de>
2026-01-26 21:49:05 +0100

    Mapclient: Refac: Added/improved functions to treat cases where user entered subc_id via form field.

--- mapclient/functions_buttonclick.js
+++ mapclient/functions_buttonclick.js
@@ -41,6 +41,7 @@ var customButtonClickBehaviour = function() {
       ogcRequestOneCoordinatePair(map, lon1, lat1, processId, processDesc, logUserAction);
 
     } else {
+      console.log("Clicked button for one subcid "+subcid1+".");
 
       // If snapping, using subc_id cannot work! Need coordinates to snap!
       if (processId.includes("snapped")) {
@@ -53,21 +54,8 @@ var customButtonClickBehaviour = function() {
         throw new Error(errmsg); // So that no OGC request is attempted
       }
 
-      console.log("Clicked button for one subcid "+subcid1+".");
       let logUserAction = "entered a subc_id (location unknown)";
-      // We have no click location...
-      clickMarker = putIconToSubcidLocation(map, logUserAction);
-      clickMarker.bindPopup("Waiting for "+processDesc+" for two subcatchments...").openPopup();
-      // Reset result field:
-      document.getElementById("responseField").innerHTML = "Response returned by server for subc_id <span class=\"code\">"+subcid1+"</span>...";
-      document.getElementById("displayGeoJSON").innerHTML = "waiting..."
-      // Construct and send HTTP request to OGC service:
-      // Param string for logging
-      var logstring = "subcid="+subcid1;
-      // Define JSON payload and send:
-      var payload_inputs_json = JSON.stringify({"inputs":{"subc_id":subcid1}})
-      _ogcRequest(clickMarker, processId, payload_inputs_json, logstring, processDesc) ;
-    }
+      ogcRequestOneSubcid(map, subc_id, processId, processDesc, logUserAction);
 
   } else if (pairs == "two") {
 
@@ -94,64 +82,28 @@ var customButtonClickBehaviour = function() {
 
     // Two subcids:
     } else if (use_subcid1 && use_subcid2) {
-      console.log("Clicked button for two subcids: "+subcid1+", "+subcid2+".");
-      // We have no click location... Putting it to a corner of the map, but it will stay at that map location...
-      // TODO: Can we later move the clickmarker somewhere?
-      let northEast = map.getBounds().getNorthEast();
-      let lat = northEast.lat;
-      let lon = northEast.lng;
-      console.log('Map Northeast: '+lon+', '+lat)
-      clickMarker = putIconToClickLocation(lon, lat, map, "entered two subc_ids (location unknown)");
-      clickMarker.bindPopup("Waiting for "+processDesc+" for two subcatchments").openPopup();
-
-      // Reset result field:
-      document.getElementById("responseField").innerHTML = "Response returned by server for subc_id <span class=\"code\">"+subcid1+"</span> to <span class=\"code\">"+subcid2+"...";
-      document.getElementById("displayGeoJSON").innerHTML = "waiting..."
-      // Construct and send HTTP request to OGC service:
-      // Param string for logging
-      var logstring = "subcid1="+subcid1+",subcid2="+subcid2;
-      // Define JSON payload and send:
-      var payload_inputs_json = JSON.stringify({"inputs":{"subc_id_start":subcid1, "subc_id_end": subcid2}})
-      _ogcRequest(clickMarker, processId, processDesc, payload_inputs_json, logstring);
+      console.log("Clicked button for two subc_ids: "+subcid1+" and "+subcid2+".");
+      let logUserAction = "entered two subc_ids (location unknown)";
+      ogcRequestTwoSubcids(map, subcid1, subcid2, processId, processDesc, logUserAction);
 
     // Mixed 1
+    // Note: Whether the user entered subc_id in the left or right field, does not matter;
+    // the exact same request is sent in both "mixed" cases. Should the order matter at some
+    // point, add another "ogcRequestTwoMixed()" function.
     } else if (use_subcid1 && !use_subcid2) {
-      // Param string for logging
-      var logstring = "subcid1="+subcid1+",lon2="+lon2+",lat2="+lat2;
-      console.log("Clicked button for mixed: "+logstring+".");
-
-      // We have no click location...
-      // WE NEED TWO CLICKMARKERS??? WIP
-      // Use known location for click marker (TODO IMPROVE)
-      clickMarker = putIconToClickLocation(lon2, lat2, map, "entered two subc_ids (location unknown)");
-      clickMarker.bindPopup("Waiting for "+processDesc+" for subcatchment and coordinate").openPopup();
-
-      // Reset result field:
-      document.getElementById("responseField").innerHTML = "Response returned by server for subc_id <span class=\"code\">"+subcid1+"</span> to <span class=\"code\">"+lon2+", "+lat2+"</span> (lon, lat, WGS84)...";
-      document.getElementById("displayGeoJSON").innerHTML = "waiting..."
-
-      // Construct and send HTTP request to OGC service:
-      // Define JSON payload and send:
-      var payload_inputs_json = JSON.stringify({"inputs":{"subc_id_start":subcid1, "lon_end": lon2, "lat_end": lat2}})
-      _ogcRequest(clickMarker, processId, processDesc, payload_inputs_json, logstring);
+      console.log("Clicked button for mixed: subcid1="+subcid1+",lon2="+lon2+",lat2="+lat2+".");
+      let logUserAction = "entered a coordinate pair and a subc_id";
+      ogcRequestTwoMixed(map, lon2, lat2, subcid1, processId, processDesc, logUserAction);
 
     // Mixed 2:
+    // Note: Whether the user entered subc_id in the left or right field, does not matter;
+    // the exact same request is sent in both "mixed" cases. Should the order matter at some
+    // point, add another "ogcRequestTwoMixed()" function.
     } else if (!use_subcid1 && use_subcid2) {
-      var logstring = "lon1="+lon1+",lat1="+lat1+",subcid2="+subcid2;
-      console.log("Clicked button for mixed: "+logstring+".");
-
-      // We have no click location...
-      // Use known location for click marker (TODO IMPROVE)
-      clickMarker = putIconToClickLocation(lon1, lat1, map, "entered mixed "+logstring);
-      clickMarker.bindPopup("Waiting for "+processDesc+" for subcatchment and coordinate").openPopup();
-      // Reset result field:
-      document.getElementById("responseField").innerHTML = "Response returned by server for <span class=\"code\">"+lon1+", "+lat1+"</span> (lon, lat, WGS84) to subc_id <span class=\"code\">"+subcid2+"</span>...";
-      document.getElementById("displayGeoJSON").innerHTML = "waiting..."
-
-      // Construct and send HTTP request to OGC service:
-      // Define JSON payload and send:
-      var payload_inputs_json = JSON.stringify({"inputs":{"lon_start": lon1, "lat_start": lat1, "subc_id_end":subcid2}})
-      _ogcRequest(clickMarker, processId, processDesc, payload_inputs_json, logstring);
+      console.log("Clicked button for mixed: lon1="+lon1+",lat1="+lat1+",subcid2="+subcid2+".");
+      let logUserAction = "entered a coordinate pair and a subc_id";
+      ogcRequestTwoMixed(map, lon1, lat1, subcid2, processId, processDesc, logUserAction);
     }
   }
 }
+
